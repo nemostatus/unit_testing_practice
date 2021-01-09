9000af94385d9e7950e27be9b4204e296862f2e5
nemostatus <mattcorralejo1@gmail.com>
2021-01-09 02:48:03 -0800

    figured it out, i needed to use the number in the loop to id the location of each element, i used that instead of indexof

--- funnyFont.js
+++ funnyFont.js
@@ -2,14 +2,26 @@ function funnyFont(string){
 const array = string.split('')
 const newarray = []
  for(let i = 0; i<array.length; i++){
-     if(array.indexOf(array[i]) === 0 || (i% 2 == 0) ){
-     newarray.push(array[i].toUpperCase())}
+     if(i === 0 || i % 2 === 0 ){
+    newarray.push(array[i].toUpperCase())}
      else {
-         newarray.push(array[i])
+        newarray.push(array[i])
      }
  }
+//x % 2 === 0 
+// const newarray = array.map(x => {
+//     if(array.indexOf(x) === 0 || array.lastIndexOf(x) === (x%2 ==0)  ){
+//       return  x.toUpperCase()
+//     }
+//     else{
+//       return  x
+//     }
+// }
+// )
+// console.log(newarray)
  return newarray.join('')
-}
+    }
+    //indexof returns first index of instance so i need to find a way to id all indexes of th element
 module.exports = funnyFont
 
 
@@ -18,5 +30,5 @@ module.exports = funnyFont
 
 //     }
 
-//how to select every other element -
+//how to select every other element 
 //how to uppercase element -
